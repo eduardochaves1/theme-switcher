e73ec015c7d7067817d3323dd7d502836a3a7b83
Eduardo Chaves <henriqueduardo2002@gmail.com>
2022-10-09 02:47:39 -0300

    Repo Creation pt.2

---
+++ script.js
@@ -0,0 +1,42 @@
+const btnToggle = document.querySelector("#btn-toggle");
+const btnToggleSwitch = document.querySelector("#btn-toggle--switch");
+const switchThemeElements = document.querySelectorAll(".theme-switcher");
+let lightMode = localStorage.getItem("lightMode");
+
+function enableLightMode () {
+  switchThemeElements.forEach((e) => {
+    e.classList.add("light-mode");
+    localStorage.setItem("lightMode", "true");
+  })
+}
+
+function disableLightMode () {
+  switchThemeElements.forEach((e) => {
+    e.classList.remove("light-mode");
+    localStorage.setItem("lightMode", "false");
+  })
+}
+
+if (lightMode === "true") {
+  enableLightMode();
+  btnToggleSwitch.classList.toggle("move-switch");
+}
+
+btnToggle.addEventListener("click", () => {
+  lightMode = localStorage.getItem("lightMode");
+  btnToggleSwitch.classList.toggle("move-switch");
+  if (lightMode === "true") {
+    disableLightMode();
+  } else {
+    enableLightMode();
+  }
+})
+
+btnToggle.addEventListener("mouseover", () => {
+  lightMode = localStorage.getItem("lightMode");
+  if (lightMode === "true") {
+    document.documentElement.style.setProperty("--slightly-move", "95%");
+  } else {
+    document.documentElement.style.setProperty("--slightly-move", "20%");
+  }
+})
